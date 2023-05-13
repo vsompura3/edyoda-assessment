d8edfd1d17db00907425e00ede14d134696281cd
Vaibhav Sompura <vsompura3@gmail.com>
2023-05-13 20:15:22 +0530

    configured tailwind config with provided assets

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <a href="/"></a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#1080ff',
+          light: '#0096ff',
+          dark: '#0048ff',
+        },
+        success: {
+          light: '#d7eddd',
+          dark: '#47ba68',
+        },
+        danger: {
+          light: '#f77171',
+          dark: '#ff0000',
+        },
+        alert: {
+          light: '#f3c163',
+          dark: '#de4313',
+        },
+        disabled: '#e7e7e7',
+        surface: {
+          100: '#d9d9d9',
+          200: '#bebebe',
+          300: '#3c4852',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
